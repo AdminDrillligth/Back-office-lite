2dc5b788da45f6eb26266de21b084f510395f063
AdminDrillligth <124135517+AdminDrillligth@users.noreply.github.com>
2023-11-10 16:29:00 +0100

    add Fields and nav into exercices creations

--- src/app/exercices-editor/exercices-editor.component.ts
+++ src/app/exercices-editor/exercices-editor.component.ts
@@ -22,32 +22,57 @@ export class ExercicesEditorComponent {
   stepService: any[]= [];
   stepNumber: number = 1;
   ngOnInit(): void {
+    let AccountOfUser = JSON.parse(localStorage.getItem('account') || '{}');
+    console.log('ACCOUNT OF USER EXERCICES EDITO :! : ', AccountOfUser);
+    this.router.navigate(['exercices-editor/exercices-details']);
     this.utilsService._templateOptions.subscribe((theme:any) => {
       console.log('THEME !: ',theme)
     });
     this.stepsService._stepNumberBase.subscribe((step:number) => {
       console.log('STEP NUMBER !: ',step)
       this.stepNumber = step;
+      if(this.stepNumber === 2 ){
+        this.router.navigate(['exercices-editor/exercices-sport-select']);
+      }
     });
     this.stepsService._titleOfexercice.subscribe((title:string) => {
       console.log('TITLE : !  !: ',title)
       this.headerTitle = title;
     });
-    let AccountOfUser = JSON.parse(localStorage.getItem('account') || '{}');
-    console.log('ACCOUNT OF USER EXERCICES EDITO :! : ', AccountOfUser);
-    this.router.navigate(['exercices-editor/exercices-details']);
+
 
   }
+
+
+
 
   goTOcreate(){
     
   }
 
   previousStep(){
+    const btnPrevious = document.querySelector<HTMLElement>('.outline-btn-previous');
+    if (btnPrevious != null) {
+      btnPrevious.style.backgroundColor = '#ff4081';
+      setTimeout(() => {
+        btnPrevious.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+      }, 1000);
+   
+    }
     this.stepsService.substractChangeNumberOfStep(this.stepNumber-1) 
   }
 
   nextStep(){
+    const btnNext = document.querySelector<HTMLElement>('.outline-btn-next');
+    if (btnNext != null) {
+      btnNext.style.backgroundColor = '#ff4081';
+      setTimeout(() => {
+        btnNext.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+      }, 1000);
+   
+    }
+
+
     this.stepsService.addChangeNumberOfStep(this.stepNumber+1)    
   }
 }
