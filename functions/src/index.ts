97fb3cf9d8d781f5ddcadb1d09e618e76a9d0763
AdminDrillligth <124135517+AdminDrillligth@users.noreply.github.com>
2023-12-13 10:45:52 +0100

    Update Login and function and Token

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,12 @@
 
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-var router = express.Router();
+// var router = express.Router();
 import { addEntry, getAllEntries, updateEntry, deleteEntry } from './entryController';
 import { addAdmin, getAdmins, updateAdmin, deleteAdmin } from './adminsController';
 import { addUser, getUsers, updateUser, deleteUser } from './usersController';
 import { addEcone, getEcones, updateEcone, deleteEcone } from './econesController';
+import { getToken } from './loggerController';
 // var methods = require("./methods");
 
 const app = express()
@@ -20,27 +21,29 @@ app.get("/",(req, res, next) => {
 });
 
 //
-router.post('/entries', addEntry)
-router.get('/entries', getAllEntries)
-router.patch('/entries/:entryId', updateEntry)
-router.delete('/entries/:entryId', deleteEntry)
+app.post('/entries', addEntry)
+app.get('/entries', getAllEntries)
+app.patch('/entries/:entryId', updateEntry)
+app.delete('/entries/:entryId', deleteEntry)
 //
-router.post('/admin', addAdmin)
-router.get('/admin', getAdmins)
-router.patch('/admin', updateAdmin)
-router.delete('/admin', deleteAdmin)
+app.post('/admin', addAdmin)
+app.get('/admin', getAdmins)
+app.patch('/admin', updateAdmin)
+app.delete('/admin', deleteAdmin)
 //
 
-router.post('/user', addUser)
-router.get('/user', getUsers)
-router.patch('/user', updateUser)
-router.delete('/user', deleteUser)
+app.post('/user', addUser)
+app.get('/user', getUsers)
+app.patch('/user', updateUser)
+app.delete('/user', deleteUser)
 
 
-router.post('/econe', addEcone)
-router.get('/econe', getEcones)
-router.patch('/econe', updateEcone)
+app.post('/econe', addEcone)
+app.get('/econe', getEcones)
+app.patch('/econe', updateEcone)
 app.delete('/econe', deleteEcone)
+
+app.get('/login', getToken)
 
 exports.app = functions.https.onRequest(app)
 
--- functions/src/loggerController.ts
+++ functions/src/loggerController.ts
@@ -1,27 +1,56 @@
-// var jwt = require("jsonwebtoken");
-// const fs = require('firebase-admin');
-import * as express from 'express';
-
-// const app = express()
-
-// const db = fs.firestore(); 
+var jwt = require("jsonwebtoken");
+const fs = require('firebase-admin');
+// import { Response } from "express"
+// import { db } from './config/firebase'
+// import * as express from 'express';
+const db = fs.firestore(); 
 // const usersDblogs = db.collection('logsofconnection'); 
-var router = express.Router();
-
-
 // const username = "snaimuh@googlemail.com";
 // const password = "Deconnecter22";
-
+const username = "bigministudiopd@gmail.com";
 // let userhandlerProfils;
 // let dataOfUsers = [];
-router.post('/login', async (req, res) => {
+const getToken = async (req:any, res:any) => {
   try {
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'entry updated successfully',
-
+    let userhandlerProfil = await db.collection('account-handler').where('email', '==', username).get();
+    let userDetail :any = '';
+    userhandlerProfil.forEach((doc:any) =>{
+      userDetail = doc.data();
     })
+    jwt.sign({ data: 'foobar'}, 'secret', { expiresIn: '1h' }, 
+      function(err:any, encoded:any) {
+        if (err) {
+          return res.status(200).json({
+            status: 'error',
+            message: 'Yo just get ERROR the token',
+            err: err
+          });
+        }else{
+          jwt.verify(encoded, 'secret', { expiresIn: '1h' },  function(err:any, decoded:any) {
+            if (err) {
+              return res.status(200).json({
+                status: 'success',
+                message: 'Yo just get ERROR the token',
+                err: err
+              });
+            }else{
+              return res.status(200).json({
+                status: 'success',
+                message: 'Yo just get the token',
+                decoded: decoded,
+                encoded:encoded,
+                user:userDetail,
+              });
+            }
+          });
+        }
+      }
+    );
   }
-  catch(error:any) { return res.status(500).json(error.message) }
-})
+  catch(error:any) { 
+    return res.status(500).json(error.message) 
+  }
+}
+
+
+export { getToken }
